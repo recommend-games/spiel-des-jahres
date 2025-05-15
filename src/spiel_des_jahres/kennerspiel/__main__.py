f0eca2528e764c070cf8f75b1b8e6ae8064e2e56
Markus Shepherd <markus@recommend.games>
2025-05-15 21:47:03 +0300

    Added basic ArgumentParser to kennerspiel.__main__

--- src/spiel_des_jahres/kennerspiel/__main__.py
+++ src/spiel_des_jahres/kennerspiel/__main__.py
@@ -1,19 +1,48 @@
 from __future__ import annotations
 
+import argparse
+import logging
 import sys
 
 import joblib
 
-from spiel_des_jahres.kennerspiel import load_games, train_model
+from spiel_des_jahres.kennerspiel import SCRAPED_DIR, load_games, train_model
 
-# TODO: add argparse
+LOGGER = logging.getLogger(__name__)
+
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Train a model to predict a game's Kennerspiel score.",
+    )
+    parser.add_argument(
+        "dest",
+        type=str,
+        help="Path to save the trained model.",
+    )
+    # TODO: more args
+    return parser.parse_args()
 
 
 def _main() -> None:
-    dest = sys.argv[1]
-    games = load_games().collect()
+    args = _parse_args()
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        stream=sys.stdout,
+    )
+    LOGGER.info(args)
+
+    LOGGER.info("Loading games from %s", SCRAPED_DIR)
+    games = load_games(SCRAPED_DIR / "bgg_GameItem.csv").collect()
+    LOGGER.info("Loaded %d games", len(games))
+
+    LOGGER.info("Training model")
     model = train_model(games.to_pandas())
-    joblib.dump(model, dest)
+
+    LOGGER.info("Saving model to <%s>", args.dest)
+    joblib.dump(model, args.dest)
+    LOGGER.info("Done.")
 
 
 if __name__ == "__main__":
